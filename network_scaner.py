04aa7dd97ec0b70a8f4e9e57f2f54542542a3f0d
Giorgi <45801652+Barbaqadze@users.noreply.github.com>
2020-03-29 21:24:42 +0400

    Add files via upload

---
+++ network_scaner.py
@@ -0,0 +1,27 @@
+import scapy.all as scapy
+from argparse import ArgumentParser
+
+def scan(ip):
+    arp_request = scapy.ARP(pdst=ip)                           #make packet and store ip address in pdst
+    broadcast   = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')         #make packet and store mac address in dst
+    arp_request_broadcast = broadcast/arp_request              #concatenate last two packets
+    answered , unanswered = scapy.srp(arp_request_broadcast , timeout=1 , verbose=False)    #use send and recieve function and store them into two list - anwered and unanswere
+    if len(answered) == 0:
+        print("No hosts in that LAN")
+        return 0
+
+    print("     IP                   MAC Address\n---------------------------------------")
+    for i in answered:
+        print(i[1].psrc , "       ", i[1].hwsrc)
+    
+
+def get_arguments():
+    parser = ArgumentParser()
+    parser.add_argument('-t' , '--target' , dest='target' , help='Target IP or IP range')
+    options = parser.parse_args()
+    return options
+
+result = get_arguments()
+scan(result.target)
+
+
